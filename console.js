c693ace242233493b2a9e140bbb5a679a782275b
franco sanchez <franco_genta@hotmail.com>
2020-08-29 18:23:09 -0300

    remove font-awesome

--- console.js
+++ console.js
@@ -5,4 +5,13 @@ window.addEventListener('load', () => {
     background: #2d2d2d;
     font-size: 1.4rem;
   `);
+
+  // const body = document.querySelector('body');
+  // const span = document.querySelector('span');
+  // const btnDark = document.querySelector('button');
+
+  // btnDark.addEventListener('click', () => {
+  //   body.classList.toggle('dark');
+  //   span.classList.toggle('dark');
+  // });
 });
