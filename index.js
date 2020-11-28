079e18322bdad1dd1ab4bb87887485f7b012559a
franco sanchez <franco_genta@hotmail.com>
2020-11-27 23:51:05 -0300

    update html

--- index.js
+++ index.js
@@ -5,17 +5,4 @@ window.addEventListener('load', () => {
     background: #2d2d2d;
     font-size: 1.6rem;
   `);
-
-  // Dark Mode
-  // const checkbox = document.querySelector('input[type="checkbox"]')
-
-  // function switchTheme(e) {
-  //   if (e.target.checked) {
-  //     document.documentElement.setAttribute('data-theme', 'dark');
-  //   } else {
-  //     document.documentElement.setAttribute('data-theme', 'light');
-  //   }
-  // }
-
-  // checkbox.addEventListener('change', switchTheme, false);
 });
