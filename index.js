017498394857e38ed630a01572b79b36b0004ce4
franco sanchez <franco_genta@hotmail.com>
2022-03-07 15:10:33 +0100

    add link articles

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const article = document.querySelector("article");
+
 window.addEventListener('load', () => {
   console.log(`
     %c¡HOLA! ¿Como estas? 👋🏻, ¿Hay algún error por aquí 👀?`,
@@ -5,4 +7,22 @@ window.addEventListener('load', () => {
     background: #2d2d2d;
     font-size: 1.6rem;
   `);
+
+  fetch("https://dev.to/api/articles?username=franqsanz")
+    .then((res) => res.json())
+    .then((data) => {
+    
+    let html = "";
+  
+    data.forEach(({ title, url, readable_publish_date }) => {
+      html += `
+        <a href="${url}" class="aLink">
+          <h2>${title}</h2>
+        </a>
+        <small>${readable_publish_date}</small>
+      `;
+
+      article.innerHTML = html;
+    });
+  });
 });
