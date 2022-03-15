1562f1a32c895da5930c7b9969bcbf5878f99906
franco sanchez <franco_genta@hotmail.com>
2022-03-15 11:48:01 -0300

    refactor: index.js file

--- js/index.js
+++ js/index.js
@@ -2,39 +2,47 @@ const article = document.querySelector("article");
 const btn = document.querySelector("button");
 
 window.addEventListener('load', () => {
+  callApi();
+
+  // event
+  btn.addEventListener('click', moreLinks);
+
   console.log(`
     %c¡HOLA! ¿Como estas? 👋🏻, ¿Hay algún error por aquí 👀?`,
     `color: #ffc700;
     background: #2d2d2d;
     font-size: 1.6rem;
   `);
+});
 
+function callApi() {
   article.innerHTML = '<div class="load"></div>';
 
   fetch("https://dev.to/api/articles?username=franqsanz")
-    .then((res) => res.json())
-    .then((data) => {
-          
-      let html = "";
-      
-      data.forEach(({ title, url, readable_publish_date }) => {
-        html += `
-          <a href="${url}" class="aLink">
-            <h2>${title}</h2>
-          </a>
-          <small>${readable_publish_date}</small>
-        `;
-        article.innerHTML = html;
-      });
-    })
-    .catch(() => {
-      article.innerHTML = '<p>No se logra conectar con la API del sitio DEV.to.</p>';
-    });
-  
-  // event
-  btn.addEventListener('click', moreLinks);
-});
+  .then((res) => res.json())
+  .then(renderHTML)
+  .catch(errCallApi);
+}
 
+function renderHTML(data) {
+  let html = "";
+      
+  data.forEach(({ title, url, readable_publish_date }) => {
+    html += `
+      <a href="${url}" class="aLink">
+        <h2>${title}</h2>
+      </a>
+      <small>${readable_publish_date}</small>
+    `;
+    article.innerHTML = html;
+  });
+}
+
+function errCallApi() {
+  article.innerHTML = '<p>No se logra conectar con la API del sitio DEV.to.</p>';
+}
+
+// event
 function moreLinks() {
   article.classList.toggle('moreLinks');
 
